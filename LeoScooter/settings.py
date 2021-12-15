6b5a7cacc5e70cde7ddd19d57f40ec0a2f9a155d
RitikSisodiy <ritik.s10120@gmail.com>
2021-12-15 16:24:38 +0530

    connect mysql database

--- LeoScooter/settings.py
+++ LeoScooter/settings.py
@@ -102,16 +102,17 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'newda',
-    #     'USER': 'ritiksisodiya',
-    #     'PASSWORD': 'ritik.s10120',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',   #my port is 3306
-    # }
+    
 }
-
+if os.environ.get('environment') == 'production':
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'newda',
+        'USER': 'ritiksisodiya',
+        'PASSWORD': 'ritik.s10120',
+        'HOST': 'localhost',
+        'PORT': '3306',   #my port is 3306
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
